4298d9ff0232917fa401cd34401eb2337283c51d
Vincent Kelvin <30566564+KLumy@users.noreply.github.com>
2023-03-11 16:58:19 +0900

    절대경로 접근을 위한 alias 설정(~)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,34 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import'],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['.'],
+        alias: {
+          '~': './src',
+        },
+      },
+    },
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        'arrow-body-style': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        '@typescript-eslint/no-unused-vars': 'warn',
+      },
+    },
+  ],
 };
